99ef75761374043617622c0d371c70cbfe575e50
thedrummonger <drummerxtmail@gmail.com>
2025-04-11 16:22:53 -0500

    Move some functions out of WinForm Utils that have nothing to do with WindowsForm

--- TDMWinUtils/WinFormUtils.cs
+++ TDMWinUtils/WinFormUtils.cs
@@ -6,25 +6,15 @@ namespace TDMWinUtils
 {
     public static class WinFormUtils
     {
-        public class ContainerItem(object? value, string display)
-        {
-            public object? Value { get; } = value;
-            public string Display { get; } = display;
-            public override string ToString() => Display;
-            public static ContainerItem[] ToContainerList<T>(IEnumerable<T> items, Func<T, string> Display) =>
-                [.. items.Select(i => new ContainerItem(i, Display(i)))];
-            public static ContainerItem[] ToContainerList<T>(IEnumerable<T> items, Func<T, object> Tags, Func<T, string> Display) =>
-                [.. items.Select(i => new ContainerItem(Tags(i), Display(i)))];
-        }
         public static T? GetSelectedContainerItem<T>(this ComboBox comboBox)
         {
-            if (comboBox.SelectedItem is ContainerItem containerItem && containerItem.Value is T value)
+            if (comboBox.SelectedItem is EnumerableUtilities.ContainerItem containerItem && containerItem.Value is T value)
                 return value;
             return default;
         }
         public static T? GetSelectedContainerItem<T>(this ListBox comboBox)
         {
-            if (comboBox.SelectedItem is ContainerItem containerItem && containerItem.Value is T value)
+            if (comboBox.SelectedItem is EnumerableUtilities.ContainerItem containerItem && containerItem.Value is T value)
                 return value;
             return default;
         }
@@ -51,11 +41,6 @@ namespace TDMWinUtils
             else
                 action();
         }
-        public static string GetDescription(this Enum value) =>
-        value.GetType().GetField(value.ToString())?.GetCustomAttributes(typeof(DescriptionAttribute), false)
-             .OfType<DescriptionAttribute>()
-             .FirstOrDefault()?.Description ?? value.ToString();
-
         public static void AppendString(this RichTextBox rtb, params ColoredString[] coloredStrings)
         {
             rtb.AppendString(() =>
